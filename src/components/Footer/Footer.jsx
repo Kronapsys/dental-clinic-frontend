1d57e12d675edcfa587b295cc4b5194578439114
Kronapsys <rsxvii@gmail.com>
2021-03-10 09:46:06 +0100

    added icons on footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import './Footer.css';
 const Footer = () => {
     return(
         <div className='footer'>
-            <div className="nombreClinica"> DentalGeek, S.L. </div>
-            <div className="direccion"> C/ Molares, 32. 46017 (Valencia, España) </div>
-            <div className="telefono">Telf: 96 357 41 59</div>
-            <div className="correo">e-mail: info@dentalgeek.com</div>
+            <div className="nombreClinica"><i class="fas fa-user-md"></i> : DentalGeek, S.L. </div>
+            <div className="direccion"><i class="fas fa-map-signs"></i> : C/ Molares, 32. 46017 (Valencia, España) </div>
+            <div className="telefono"><i class="fas fa-phone-alt"></i> : 96 357 41 59</div>
+            <div className="correo"><i class="far fa-envelope"></i> : info@dentalgeek.com</div>
             <div className="redesSociales">
                 <div className="twitter">
                     <img src='../img/twitter.png'></img>
