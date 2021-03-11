94d4ecf42b63118f0def44274bd52bc945792c5a
Kronapsys <rsxvii@gmail.com>
2021-03-11 16:48:07 +0100

    re uploading last commits because gitHub fails

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -5,7 +5,20 @@ const Home = () => {
     
     return(
         <div className='home'>
-           
+           <div className="homeHorario">
+            <label className='lblHorario'>HORARIO:</label> <br/>
+            <label className='lblDias'>L: 09:00 a 18:00</label>
+            <label className='lblDias'>M: 09:00 a 18:00</label>
+            <label className='lblDias'>X: 09:00 a 18:00</label>
+            <label className='lblDias'>J: 09:00 a 18:00</label>
+            <label className='lblDias'>V: 09:00 a 14:00</label>
+            <label className='lblDias'>S: 10:00 a 13:00</label>
+            
+           </div>
+
+           <div className="homeServicios">
+               
+           </div>
         </div>
     );
 };
