0618ee8e9dff70ed97bcf2d0e18fd5c4ac704615
Kronapsys <rsxvii@gmail.com>
2021-03-11 20:53:48 +0100

    home page with info

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
-    
-    return(
-        <div className='home'>
-           <div className="homeHorario">
-            <label className='lblHorario'>HORARIO:</label> <br/>
-            <label className='lblDias'>L: 09:00 a 18:00</label>
-            <label className='lblDias'>M: 09:00 a 18:00</label>
-            <label className='lblDias'>X: 09:00 a 18:00</label>
-            <label className='lblDias'>J: 09:00 a 18:00</label>
-            <label className='lblDias'>V: 09:00 a 14:00</label>
-            <label className='lblDias'>S: 10:00 a 13:00</label>
-            
-           </div>
+  return (
+    <div className="home">
+      <div className="homeHorario">
+        <label className="lblHorario">HORARIO:</label> <br />
+        <label className="lblDias">L: 09:00 a 18:00</label>
+        <label className="lblDias">M: 09:00 a 18:00</label>
+        <label className="lblDias">X: 09:00 a 18:00</label>
+        <label className="lblDias">J: 09:00 a 18:00</label>
+        <label className="lblDias">V: 09:00 a 14:00</label>
+        <label className="lblDias">S: 10:00 a 13:00</label>
+      </div>
 
-           <div className="homeServicios">
-               
-           </div>
+      <div className="homeServicios">
+        <div className="homeServicios1">
+          <div className="homeServicios1Izq">· Implantología</div>
+          <div className="homeServicios1Der">
+            Implantes de carga inmediata <br/>
+            Puente dental sobre implantes <br/>
+            Implante dental unitario <br/>
+            
+          </div>
         </div>
-    );
+
+        <div className="homeServicios2">
+          <div className="homeServicios2Izq">· Prótesis dentales</div>
+          <div className="homeServicios2Der">
+            Prótesis dental fija sobre implantes <br/>
+            Limpieza de prótesis dental <br/>
+            Prótesis dental fija <br/>
+            
+          </div>
+        </div>
+
+        <div className="homeServicios3">
+          <div className="homeServicios3Izq">· Ortodoncia</div>
+          <div className="homeServicios3Der">
+            Ortodoncia invisible <br/>
+            Ortodoncia infantil <br/>
+            Brackets <br/>
+          </div>
+        </div>
+
+        <div className="homeServicios4">
+          <div className="homeServicios4Izq">· Odontología</div>
+          <div className="homeServicios4Der">
+            Test genético bacteriológico periodontal <br/>
+            Periodoncia y Endodoncia <br/>
+            Antirronquidos <br/>
+            Halitosis <br/>
+            Caries <br/>
+            
+            
+            
+          </div>
+        </div>
+
+        <div className="homeServicios5">
+          <div className="homeServicios5Izq">· Estética dental</div>
+          <div className="homeServicios5Der">
+            Limpieza dental avanzada <br/>
+            Blanqueamiento dental <br/>
+            Carillas dentales <br/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
