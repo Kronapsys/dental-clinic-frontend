e4b2e3304578a6563cd03db9ab619d126e9fa17a
Kronapsys <rsxvii@gmail.com>
2021-03-18 13:48:46 +0100

    added redirection on button make an appointment to profile

--- src/containers/Appointment/Appointment.jsx
+++ src/containers/Appointment/Appointment.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { ADD_APPOINTMENT } from '../../redux/types/appointmentType.js';
 import './Appointment.css';
 
 const Appointment = (props) => {
 
-    // let history = useHistory();
+    let history = useHistory();
 
     const [dataAppointment, setAppointment] = useState({
         date: "",
@@ -36,31 +36,22 @@ const Appointment = (props) => {
 
         let result = await axios.post("http://localhost:3001/appointments", body);
         props.dispatch({ type: ADD_APPOINTMENT, payload: result.data });
+        history.push('profile');
         console.log(result.data);
     }
 
     return (
         <div className="appointment">
-
             <div className="rellenoLogin"></div>
             <div className='formLogIn'>
                 Fecha: <input className='emailInput' type='date' name='date' title='Date' onChange={handleState} />
                 Descripcion: <input className='passwordInput' type='text' name='description' title='description'  lenght='50' onChange={handleState} />
-                <input hidden="true" className='passwordInput' type='number' name='customerId' title='customerId'  lenght='50' onChange={handleState} /* value={props.user.customer.id} */ />
+                <input hidden="true" className='passwordInput' type='number' name='customerId' title='customerId'  lenght='50' onChange={handleState} />
                 <button className='btnEntrar' type='submit' onClick={createAppointment}>PEDIR CITA</button>
             </div>
         </div>
     )
 };
-
-
-
-
-
-
-
-
-
 
 const mapStateToProps = state =>{
     return{
