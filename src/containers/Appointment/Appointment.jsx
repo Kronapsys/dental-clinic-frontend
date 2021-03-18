3e5f83efee441a91025c0eb1ee73355a81a0af3d
Kronapsys <rsxvii@gmail.com>
2021-03-18 13:27:42 +0100

    user can get an appointent, implemented 95% by vicGeo :)

--- src/containers/Appointment/Appointment.jsx
+++ src/containers/Appointment/Appointment.jsx
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 import { ADD_APPOINTMENT } from '../../redux/types/appointmentType.js';
 import './Appointment.css';
 
-
 const Appointment = (props) => {
 
     // let history = useHistory();
@@ -32,19 +31,13 @@ const Appointment = (props) => {
         const body = {
             date: dataAppointment.date,
             description: dataAppointment.description,
-            customerId: dataAppointment.customerId
-
-
+            customerId: props.user.customer.id
         };
-
 
         let result = await axios.post("http://localhost:3001/appointments", body);
         props.dispatch({ type: ADD_APPOINTMENT, payload: result.data });
         console.log(result.data);
-
     }
-
-
 
     return (
         <div className="appointment">
@@ -53,14 +46,11 @@ const Appointment = (props) => {
             <div className='formLogIn'>
                 Fecha: <input className='emailInput' type='date' name='date' title='Date' onChange={handleState} />
                 Descripcion: <input className='passwordInput' type='text' name='description' title='description'  lenght='50' onChange={handleState} />
-                ID: <input className='passwordInput' type='number' name='customerId' title='customerId'  lenght='50' onChange={handleState} />
+                <input hidden="true" className='passwordInput' type='number' name='customerId' title='customerId'  lenght='50' onChange={handleState} /* value={props.user.customer.id} */ />
                 <button className='btnEntrar' type='submit' onClick={createAppointment}>PEDIR CITA</button>
-
             </div>
         </div>
     )
-
-
 };
 
 
@@ -72,4 +62,9 @@ const Appointment = (props) => {
 
 
 
-export default connect()(Appointment);
+const mapStateToProps = state =>{
+    return{
+        user: state.userReducer.user
+    }
+};
+export default connect(mapStateToProps)(Appointment);
